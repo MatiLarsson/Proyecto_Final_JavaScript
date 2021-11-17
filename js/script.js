19b7fa80e281c508c72323784303745ad8caf7e7
Matias Larsson <larssonmts@gmail.com>
2021-11-17 13:43:24 -0300

    se desarrolla la web y se pule el codigo js

--- js/script.js
+++ js/script.js
@@ -13,14 +13,37 @@ let total = 0;
 const idUsuario = idUsuarios.length + 1;
 let hoy = new Date();
 let fechaActual = hoy.getDate() + '/' + (hoy.getMonth() + 1) + '/' + hoy.getFullYear();
+// Se crea la función para calcular el total a pagar
+function CalculartotalCarrito() {
+    for (let item of itemsEnCarrito) {
+        for (let producto of productos) {
+            if (producto.idProducto === item.idProducto) {
+                let resultadoItem = item.cantidad * producto.precio;
+                total += resultadoItem;
+                console.log(`Producto código: ${producto.idProducto} ===> ${item.cantidad} unidades x USD ${producto.precio} = USD ${resultadoItem}`);
+            }
+        }
+    }
+    console.log(`Total del carrito USD ${total}`);
+}
+// Se crea la función para mantener acutalizado el stock luego de una compra
+function actualizarStock() {
+    for (let item of itemsEnCarrito) {
+        for (let producto of productos) {
+            if (producto.idProducto === item.idProducto) {
+                producto.vender(item.cantidad);
+                console.log(`Se han removido ${item.cantidad} unidades del stock del producto código: ${producto.idProducto}.`);
+            }
+        }
+    }
+}
 // Se almacena el id del usuario actual en el array idUsuarios []
 idUsuarios.push(idUsuario);
 // Se crean las clases del sitio y sus métodos
 class Producto {
-    constructor(idProducto, nombre, unidad, precio, stock) {
+    constructor(idProducto, nombre, precio, stock) {
         this.idProducto = parseInt(idProducto);
         this.nombre = nombre.toUpperCase();
-        this.unidad = unidad.toUpperCase();
         this.precio = parseFloat(precio);
         this.stock = parseInt(stock);
     }
@@ -32,12 +55,11 @@ class Producto {
     }
 }
 class Usuario {
-    constructor(id, nombre, apellido, email, telefono, direccionDeEnvio) {
+    constructor(id, nombre, apellido, email, direccionDeEnvio) {
         this.id = id;
         this.nombre = nombre.toUpperCase();
         this.apellido = apellido.toUpperCase();
         this.email = email;
-        this.telefono = parseInt(telefono);
         this.direccionDeEnvio = direccionDeEnvio.toUpperCase();
     }
 }
@@ -69,26 +91,28 @@ class Venta {
     }
 }
 // Se agragan los productos de la tienda
-productos.push(new Producto(1, "envío", "km", 200, 9999));
-productos.push(new Producto(2, "arena gruesa", "m3",1500, 100));
-productos.push(new Producto(3, "arena fina", "m3", 2000, 80));
-productos.push(new Producto(4, "bolsa de cemento", "50 kg", 1000, 20));
-productos.push(new Producto(5, "granza", "m3", 2500, 50));
+productos.push(new Producto(1, "macbook pro", 2299, 10));
+productos.push(new Producto(2, "macbook air", 1449, 10));
+productos.push(new Producto(3, "imac", 2999, 10));
+productos.push(new Producto(4, "iphone 13 pro max", 1299, 10));
+productos.push(new Producto(5, "iphone 13 pro", 1199, 10));
+productos.push(new Producto(6, "iphone 13", 999, 10));
+productos.push(new Producto(7, "iphone 12", 899, 10));
+productos.push(new Producto(8, "iphone SE", 499, 10));
+productos.push(new Producto(9, "iphone 11", 549, 10));
 // Se actualiza su precio para sumarle el IVA
 for (const producto of productos) {
     producto.sumaIva();
 }
-
 // Comienzo de interacción con el usuario
-confirm ("¡Bienvenidos a nuestra tienda virtual de insumos para su obra!");
+confirm ("¡Bienvenidos a nuestra tienda online!");
 confirm ("Antes que nada, te pediremos algunos datos...");
 // Se crea un nuevo usuario al cargar el sitio y se lo carga como objeto en el array usuarios []
 const nombreUsuario = prompt("Ingrese nombre: ");
 const apellidoUsuario = prompt("Ingrese apellido: ");
 const emailUsuario = prompt("Ingrese su email: ");
-const telefonoUsuario = prompt("Ingrese su teléfono: ");
 const direccionUsuario = prompt("Ingrese su dirección para envíos: ")
-usuarios.push(new Usuario(idUsuario, nombreUsuario, apellidoUsuario, emailUsuario, telefonoUsuario, direccionUsuario));
+usuarios.push(new Usuario(idUsuario, nombreUsuario, apellidoUsuario, emailUsuario, direccionUsuario));
 confirm ("A continuación podrás agregar productos al carrito y solicitar el envío a tu domicilio.");
 confirm ("Vamos a iniciar tu pedido...");
 // Se crea un nuevo carrito para este usuario
@@ -103,21 +127,12 @@ do {
 } while (confirm("¿Desea continuar comprando?"));
 alert("Abra su consola para corroborar los productos en su carrito.");
 console.log("Su carrito contiene: ");
-for (const item of itemsEnCarrito) {
-    console.log(`${item.cantidad} unidades de producto ${item.codigo}.`);
-}
+console.log(CalculartotalCarrito());
 if (!confirm("Click en 'Aceptar' para pagar.")) {
     console.log("Lamentamos que se haya arrepentido. Puede volver cuando quiera.");
 } else {
-    for (let item of itemsEnCarrito) {
-        for (let producto of productos) {
-            if (producto.idProducto === item.idProducto) {
-                producto.vender(item.cantidad);
-                total += (item.cantidad * producto.precio);
-            }
-        }
-    }
-    alert(`Se debitará un total de $${total} de su tarjeta`);
+    alert(`Se debitará un total de USD ${total} de su tarjeta`);
+    console.log(actualizarStock());
     ventas.push(new Venta(fechaActual, itemsEnCarrito, idUsuario, direccionUsuario));
     console.log("¡Gracias por comprar en nuestra tienda!");
 }
