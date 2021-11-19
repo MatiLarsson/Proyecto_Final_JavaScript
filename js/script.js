0494f0e413739f2eadd798d0ff66b192f10c571e
Matias Larsson <larssonmts@gmail.com>
2021-11-19 11:24:43 -0300

    se pule el script

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
 //  Se crea el arrary de productos
-const productos = [];
+let productos = [];
 // Se crea el array de usuarios de la tienda
-const usuarios = [];
-// Se crea un array con los IDs de los usuarios hasta el momento
-const idUsuarios = [];
+let usuarios = [];
 // Se crea un array con los items cargados en el Carrito
-const itemsEnCarrito = [];
+let itemsEnCarrito = [];
 // Se crea el array de ventas para llevar un registro de las mismas
-const ventas = [];
+let ventas = [];
 // Se crean las variables y constantes globales
 let total = 0;
-const idUsuario = idUsuarios.length + 1;
 let hoy = new Date();
 let fechaActual = hoy.getDate() + '/' + (hoy.getMonth() + 1) + '/' + hoy.getFullYear();
+// Se crea la función para imprimir en consoola
+function mostrar(elemento) {
+    console.log(elemento);
+}
 // Se crea la función para calcular el total a pagar
-function CalculartotalCarrito() {
+function calcularCarrito() {
     for (let item of itemsEnCarrito) {
         for (let producto of productos) {
-            if (producto.idProducto === item.idProducto) {
-                let resultadoItem = item.cantidad * producto.precio;
-                total += resultadoItem;
-                console.log(`Producto código: ${producto.idProducto} ===> ${item.cantidad} unidades x USD ${producto.precio} = USD ${resultadoItem}`);
+            if (producto.idP === item.idP) {
+                let sumatoriaItem = item.cantidad * producto.precio;
+                total += sumatoriaItem;
+                console.log(`Producto código: ${producto.idP} ===> ${item.cantidad} unidades x USD ${producto.precio} = USD ${sumatoriaItem}`);
             }
         }
     }
@@ -28,111 +29,115 @@ function CalculartotalCarrito() {
 }
 // Se crea la función para mantener acutalizado el stock luego de una compra
 function actualizarStock() {
+    console.log("Actualización de stock:")
     for (let item of itemsEnCarrito) {
         for (let producto of productos) {
-            if (producto.idProducto === item.idProducto) {
+            if (producto.idP === item.idP) {
                 producto.vender(item.cantidad);
-                console.log(`Se han removido ${item.cantidad} unidades del stock del producto código: ${producto.idProducto}.`);
+                console.log(`Se han removido ${item.cantidad} unidades del stock del producto código: ${producto.idP}.`);
             }
         }
     }
 }
-// Se almacena el id del usuario actual en el array idUsuarios []
-idUsuarios.push(idUsuario);
 // Se crean las clases del sitio y sus métodos
 class Producto {
-    constructor(idProducto, nombre, precio, stock) {
-        this.idProducto = parseInt(idProducto);
+    constructor(nombre, precio, stock) {
         this.nombre = nombre.toUpperCase();
         this.precio = parseFloat(precio);
         this.stock = parseInt(stock);
+        this.idP = Producto.ID;
+        Producto.ID++;
     }
+    static ID = 0
     sumaIva() {
         this.precio = this.precio * 1.21;
     }
-    vender(cantidadVendida) {
-        this.stock -= cantidadVendida;
+    vender(cantidad) {
+        this.stock -= cantidad;
+    }
+    reponer(cantidad) {
+        this.stock += cantidad;
     }
 }
 class Usuario {
-    constructor(id, nombre, apellido, email, direccionDeEnvio) {
-        this.id = id;
+    constructor(nombre, apellido, email, direccionDeEnvio) {
         this.nombre = nombre.toUpperCase();
         this.apellido = apellido.toUpperCase();
         this.email = email;
         this.direccionDeEnvio = direccionDeEnvio.toUpperCase();
+        this.carrito = itemsEnCarrito;
+        this.idU = Usuario.ID;
+        Usuario.ID++
     }
+    static ID = 0;
 }
-class Carrito {
-    constructor(id, items) {
-        this.id = id;
-        this.items = items;
+// Se crea una función para cargar un nuevo usuario y almacenarlo en el array de usuarios[]
+function nuevoUsuario() {
+    const nombreUsuario = prompt("Ingrese nombre: ");
+    const apellidoUsuario = prompt("Ingrese apellido: ");
+    const emailUsuario = prompt("Ingrese su email: ");
+    const direccionUsuario = prompt("Ingrese su dirección para envíos: ");
+    usuarios.push(new Usuario(nombreUsuario, apellidoUsuario, emailUsuario, direccionUsuario));
+}
+class Item {
+    constructor(idP, cantidad) {
+        this.idP = parseInt(idP);
+        this.cantidad = parseInt(cantidad);
     }
     agregar(item) {
-        this.items.push(item);
+        this.itemsEnCarrito.push(item);
     }
     quitar(item) {
-        this.items.pop(item);
-    }
-}
-// La siguiente clase se crea específicamente para uso dentro del array itemsEnCarrito, propiedad de los objetos de la clase Carrito
-class Item {
-    constructor(idProducto, cantidad) {
-        this.idProducto = parseInt(idProducto);
-        this.cantidad = parseInt(cantidad);
+        this.itemsEnCarrito.pop(item);
     }
 }
 class Venta {
-    constructor(fecha, items, id, direccionDeEnvio) {
-        this.fecha = fecha;
-        this.items = items;
-        this.id = id;
-        this.direccionDeEnvio = direccionDeEnvio.toUpperCase();
+    constructor() {
+        this.fecha = fechaActual;
+        this.itemsEnCarrito = itemsEnCarrito;
+        this.total = total;
+        this.usuario = usuarios[usuarios.length - 1];
     }
 }
-// Se agragan los productos de la tienda
-productos.push(new Producto(1, "macbook pro", 2299, 10));
-productos.push(new Producto(2, "macbook air", 1449, 10));
-productos.push(new Producto(3, "imac", 2999, 10));
-productos.push(new Producto(4, "iphone 13 pro max", 1299, 10));
-productos.push(new Producto(5, "iphone 13 pro", 1199, 10));
-productos.push(new Producto(6, "iphone 13", 999, 10));
-productos.push(new Producto(7, "iphone 12", 899, 10));
-productos.push(new Producto(8, "iphone SE", 499, 10));
-productos.push(new Producto(9, "iphone 11", 549, 10));
-// Se actualiza su precio para sumarle el IVA
-for (const producto of productos) {
-    producto.sumaIva();
+// Se crea la función para agregar inicialmente productos a la tienda
+function add(nombre, precio, cantidad) {
+    productos.push(new Producto(nombre, precio, cantidad));
+    productos[productos.length - 1].sumaIva();
 }
+// Se agragan los productos de la tienda
+add("macbook pro", 2299, 10);
+add("macbook air", 1449, 10);
+add("imac", 2999, 10);
+add("iphone 13 pro max", 1299, 10);
+add("iphone 13 pro", 1199, 10);
+add("iphone 13", 999, 10);
+add("iphone 12", 899, 10);
+add("iphone SE", 499, 10);
+add("iphone 11", 549, 10);
 // Comienzo de interacción con el usuario
 confirm ("¡Bienvenidos a nuestra tienda online!");
 confirm ("Antes que nada, te pediremos algunos datos...");
 // Se crea un nuevo usuario al cargar el sitio y se lo carga como objeto en el array usuarios []
-const nombreUsuario = prompt("Ingrese nombre: ");
-const apellidoUsuario = prompt("Ingrese apellido: ");
-const emailUsuario = prompt("Ingrese su email: ");
-const direccionUsuario = prompt("Ingrese su dirección para envíos: ")
-usuarios.push(new Usuario(idUsuario, nombreUsuario, apellidoUsuario, emailUsuario, direccionUsuario));
+nuevoUsuario();
 confirm ("A continuación podrás agregar productos al carrito y solicitar el envío a tu domicilio.");
 confirm ("Vamos a iniciar tu pedido...");
-// Se crea un nuevo carrito para este usuario
-const carritoUsuarioID = new Carrito(idUsuario, itemsEnCarrito);
 do {
-    let codigo = prompt("Ingrese el código del producto que quiere agregar a su carrito: ");
-    let cantidad = prompt(`Ingrese cantidad del producto ${codigo} que quiere comprar: `);
-    carritoUsuarioID.agregar(new Item(codigo, cantidad));
+    let codigo = parseInt(prompt("Ingrese el código del producto que quiere agregar a su carrito: "));
+    let cantidad = parseInt(prompt(`Ingrese cantidad del producto ${codigo} que quiere comprar: `));
+    itemsEnCarrito.push(new Item(codigo, cantidad));
     if (!confirm(`Se han agregado ${cantidad} unidades de producto ${codigo} a su carrito de compras. "ACEPTAR" para confirmar, "CANCELAR" para anular.`)) {
-        carritoUsuarioID.quitar(itemsEnCarrito[itemsEnCarrito.length - 1]);
+        itemsEnCarrito.quitar(itemsEnCarrito[itemsEnCarrito.length - 1]);
     }
 } while (confirm("¿Desea continuar comprando?"));
 alert("Abra su consola para corroborar los productos en su carrito.");
-console.log("Su carrito contiene: ");
-console.log(CalculartotalCarrito());
-if (!confirm("Click en 'Aceptar' para pagar.")) {
-    console.log("Lamentamos que se haya arrepentido. Puede volver cuando quiera.");
+mostrar("Su carrito contiene: ");
+calcularCarrito();
+if (!confirm("Click en 'Aceptar' para pagar, o en 'Cancelar' para eliminar todos los items del carrito.")) {
+    itemsEnCarrito.splice();
+    mostrar("Lamentamos que se haya arrepentido. Puede volver cuando quiera.");
 } else {
     alert(`Se debitará un total de USD ${total} de su tarjeta`);
-    console.log(actualizarStock());
-    ventas.push(new Venta(fechaActual, itemsEnCarrito, idUsuario, direccionUsuario));
-    console.log("¡Gracias por comprar en nuestra tienda!");
+    actualizarStock();
+    ventas.push(new Venta());
+    mostrar("¡Gracias por comprar en nuestra tienda!");
 }
