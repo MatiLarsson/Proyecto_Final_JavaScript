8b2e6a70ad7636b873ecb67c8fc081b067e9829b
Matias Larsson <larssonmts@gmail.com>
2021-12-27 10:42:44 -0300

    se añade descripción del proyecto

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+/* Descripción del proyecto:
+
+- Se implementa una tienda virtual de productos apple utilizando el framework bootstrap, javascript nativo y JQuery.
+- Se pude añadir productos al carrito de manera anónima o registrado. Al querer terminar el proceso de compra se requerirá al usuario
+que se registre y luego proceda a pagar.
+- Los datos de usuario, ya sea éste anónimo o registrado previamente se llevan en el local storage. Al cargar la página se recuperan los
+mismos si es que existen. Si no existe info previa de algún usuario, se generará un usuario anónimo, el cual podrá reescribirse luego al
+registrarse (los datos de carrito y de historial de compras no se pierden).
+- En cualquier momento se puede reescribir los datos de usuario clickeando en el nombre de usuario en el encabezado.
+- El botón con ícono de carrito lleva un contador de productos "en carrito".
+- El carrito cuenta con funcionalidades de "Vaciar carrito" y "Eliminación del grupo un mismo producto", y "modificacion de la cantidad"
+de un determinado producto con los iconos + y -.
+- Al terminar una compra, los datos de la misma se guardan en un historial de compras creado para el usuario actual. El mismo y su detalle
+de cada uno de los productos puede verse en la modal que se ejectua al clickear sobre el botón "Mis compras" en el encabezado.
+- Se implementan llamadas GET y POST en:
+Archivo main.js linea 61 (GET)
+Archivo logEvents.js linea 112 (POST)
+Archivo shopEvents.js línea 28 (POST)
+*/
+
 // Variables globales:
 let productos = [];
 let usuarios = [];
